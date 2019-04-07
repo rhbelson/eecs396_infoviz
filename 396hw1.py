3e0185e6a289256008eb1a55e2b86de299999ab6
Robbie Belson <rhbelson@gmail.com>
2019-04-06 22:20:18 -0500

    added scripts and edited description

--- 396hw1.py
+++ 396hw1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
  
 
 f=open("antibiotics_data.csv","r")
@@ -53,7 +54,7 @@ opacity = 0.8
 rects1 = plt.bar(index, p, bar_width,
 alpha=opacity,
 color=staining,
-label='Penicillin without Gram Staining')
+label='Penicillin | Gram Negative')
 
 staining2=[]
 for p in range(len(staining)):
@@ -65,7 +66,7 @@ for p in range(len(staining)):
 rects2 = plt.bar(index + bar_width, s, bar_width,
 alpha=opacity,
 color=staining2,
-label='Streptomycin without Gram Staining')
+label='Streptomycin | Gram Negative')
 
 
 staining3=[]
@@ -79,13 +80,35 @@ for p in range(len(staining)):
 rects3 = plt.bar(index + 2*bar_width, n, bar_width,
 alpha=opacity,
 color=staining3,
-label='Neomycin without Gram Staining')
- 
+label='Neomycin | Gram Negative')
+
+patterns = ('', '', '\\', '\\', '', '', '', '', '', '', '','\\', '\\','\\', '\\',
+	'\\')
+for bar, pattern in zip(rects1, patterns):
+    bar.set_hatch(pattern)
+for bar, pattern in zip(rects2, patterns):
+    bar.set_hatch(pattern)
+for bar, pattern in zip(rects3, patterns):
+    bar.set_hatch(pattern)
+
+
+
+
 plt.xlabel('Antibiotic Performance')
 plt.ylabel('Minimum Inhibitory Concentration (MIC)')
 plt.title('MIC of WW2 Antibiotics by Bacteria')
 plt.xticks(index + bar_width, bacteria)
-plt.legend()
+
+
+red_patch = mpatches.Patch(facecolor='#3B1DDA',edgecolor='black',hatch='///', label='Penicillin | Gram Positive')
+blue_patch = mpatches.Patch(facecolor='#149E2B',edgecolor='black',hatch='///',label='Streptomycin | Gram Positive')
+green_patch = mpatches.Patch(facecolor='#D11548',edgecolor='black',hatch='///', label='Neomycin | Gram Positive')
+red_patch2 = mpatches.Patch(facecolor='#7BC9ED',edgecolor='black',hatch='///', label='Penicillin | Gram Negative')
+blue_patch2 = mpatches.Patch(facecolor='#57E66F',edgecolor='black',hatch='///',label='Streptomycin | Gram Negative')
+green_patch2 = mpatches.Patch(facecolor='#F4D1DB',edgecolor='black',hatch='///', label='Neomycin | Gram Negative')
+
+
+plt.legend(handles=[red_patch, blue_patch, green_patch,red_patch2, blue_patch2, green_patch2])
 
 for tick in ax.xaxis.get_major_ticks():
    tick.label.set_fontsize(7) 
